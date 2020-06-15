2041139bfb4fb3b54a7596cbaac3a8589dec251f
jie <48710834+aichilvrouhuoshao@users.noreply.github.com>
2020-06-15 16:34:55 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ loss1 = plt.plot(x1,c, c='c', ls=':',lw=0.5, label='c_loss' )
 plt.ylim(0,6000)
 plt.xlabel('Epoch',fontsize = 12)
 plt.ylabel('c_loss', fontsize = 12)
-plt.grid(b = True, color = 'k', linestyle = '--', alpha = 0.2)#alpha 为透明度
+plt.grid(b = True, color = 'k', linestyle = '--', alpha = 0.2)#alpha is the transparency
 plt.legend(fontsize = 10)
 ax = plt.gca()
 ax.spines['top'].set_visible(False)
@@ -19,7 +19,7 @@ plt.savefig('./loss_plot/tc100_td1_trainstep400/c_loss.tif')
 plt.show()
 
 
-# #画d_loss
+# #draw the d_loss
 
 # d = np.load('d_data.npy') #size d = 171
 # x2 = np.linspace(0,171,171)
@@ -27,16 +27,16 @@ plt.show()
 # plt.legend()
 # plt.show()
 
-#画g_loss
+#draw the g_loss
 g = np.load('g_data.npy') #size g = 51129
 x3 = np.linspace(100,400,51129)
 plt.plot(x3,g, c='orange', ls=':',lw=0.5, label='g_loss')
 plt.xlabel('Epoch',fontsize = 12)
 plt.ylabel('g_loss', fontsize = 12)
-plt.grid(b = True, color = 'w', linestyle = '-', alpha = 1)#alpha 为透明度
+plt.grid(b = True, color = 'w', linestyle = '-', alpha = 1)#alpha is the transparency
 
 ax = plt.gca()
-ax.patch.set_facecolor("gray") #设置背景颜色
+ax.patch.set_facecolor("gray") #set the color of playground
 ax.patch.set_alpha(0.2)
 plt.legend(fontsize = 10)
 ax.spines['top'].set_visible(False)
