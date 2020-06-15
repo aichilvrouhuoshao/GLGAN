3cba8d47073d531361e4c3cca975b7cad14919ea
jie <48710834+aichilvrouhuoshao@users.noreply.github.com>
2020-06-15 16:39:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ def erase_img(args, img):
         img_show = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         cv2.imshow('image',img_show)
         k = cv2.waitKey(1) & 0xFF
-        if k == ord('q'): #按q关闭图片界面
+        if k == ord('q'): #press 'q' to close the mask process
             break
 
     test_img = cv2.resize(img, (args.input_height, args.input_width))/127.5 - 1
@@ -67,13 +67,13 @@ def test(args, sess, model):
     ckpt_name = str(last_ckpt)
     print("Loaded model file from " + ckpt_name)
     
-    img = cv2.imread('./test_result_orig/12_orig.jpg')    #读入测试图片路径
+    img = cv2.imread('./test_result_orig/12_orig.jpg')    #read the path of testing images
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     orig_test = cv2.resize(img, (args.input_height, args.input_width))/127.5 - 1
     # print(orig_test.shape) #(64, 64, 3)
-    orig_test = np.tile(orig_test[np.newaxis,...],[args.batch_size,1,1,1]) #将图片格式(64, 64, 3)转化为(64, 64, 64, 3)？
+    orig_test = np.tile(orig_test[np.newaxis,...],[args.batch_size,1,1,1]) #transfer the image from (64, 64, 3)to(64, 64, 64, 3)
     # print(orig_test.shape) #(64, 64, 64, 3)
-    orig_test = orig_test.astype(np.float32)  #orig_test为均为1 的4维张量
+    orig_test = orig_test.astype(np.float32)  #orig_test is the 4-D tensor filled with 1
     # print(orig_test.shape) #(64, 64, 64, 3)
     orig_w, orig_h = img.shape[0], img.shape[1]
     test_img, mask = erase_img(args, img)
@@ -87,17 +87,8 @@ def test(args, sess, model):
     res_img = sess.run(model.test_res_imgs, feed_dict={model.single_orig:orig_test,
                                                        model.single_test:test_img,
                                                        model.single_mask:mask})
-    #下面这段注释是原版程序，用于直接显示结果界面，下面的替换代码是参考 make_maask.py分别存储三张图片
-    # orig = cv2.resize(orig_test[0], (orig_h//2, orig_w//2)) #orig_test[0] 取4维张量中第一张三维图，0-63共64维
-    # test = cv2.resize(test_img[0], (orig_h//2, orig_w//2))
-    # recon = cv2.resize(res_img[0], (orig_h//2, orig_w//2))
-    #
-    # res = np.hstack([orig,test,recon])
-    # res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
-    #
-    # cv2.imshow("result", res)
-    # cv2.waitKey(0)
 
+    # save the results
     orig_test = cv2.cvtColor(orig_test[0], cv2.COLOR_BGR2RGB)
     test_img = cv2.cvtColor(test_img[0], cv2.COLOR_BGR2RGB)
     res_img = cv2.cvtColor(res_img[0], cv2.COLOR_BGR2RGB)
