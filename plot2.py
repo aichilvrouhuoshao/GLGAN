50f5873ef479ce0dbfe44db1c01a81122de462c2
jie <48710834+aichilvrouhuoshao@users.noreply.github.com>
2020-06-15 16:36:44 +0800

    Update plot2.py

--- plot2.py
+++ plot2.py
@@ -2,35 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import MultipleLocator
 
-# # 求closs在每个epoch中的平均值
-# c_ave = []
-# c = np.load('./loss_plot/tc300_td1_trainstep1000/c_data.npy') #size c = 17101
-# for i in range(300):
-#     sum = 0
-#     for j in range(171):
-#         sum = sum + c[171*i+j]
-#     ave = sum / 171
-#     c_ave.append(ave)
-# # print(c_ave)
-# np.save('./loss_plot/tc300_td1_trainstep1000/c_ave.npy', c_ave)
-# # 画closs的图
-# x1 = np.linspace(0,300,300)
-# loss1 = plt.plot(x1,c_ave, c='c', marker='o', ms= 2, lw=0.5, label='c_loss' )
-# plt.ylim(0,6000)
-# plt.xlabel('Epoch',fontsize = 12)
-# plt.ylabel('c_loss', fontsize = 12)
-# plt.grid(b = True, color = 'k', linestyle = '--', alpha = 0.2)#alpha 为透明度
-# plt.legend(fontsize = 10)
-# ax = plt.gca()
-# ax.spines['top'].set_visible(False)
-# # ax.spines['bottom'].set_visible(False)
-# ax.spines['right'].set_visible(False)
-# # ax.spines['left'].set_visible(False)
-# # plt.savefig('./loss_plot/tc100_td1_trainstep400/c_loss_ave.tif')
-# plt.show()
+# calculate the average closs in every epoch
+c_ave = []
+c = np.load('./loss_plot/tc300_td1_trainstep1000/c_data.npy') #size c = 17101
+for i in range(300):
+    sum = 0
+    for j in range(171):
+        sum = sum + c[171*i+j]
+    ave = sum / 171
+    c_ave.append(ave)
+# print(c_ave)
+np.save('./loss_plot/tc300_td1_trainstep1000/c_ave.npy', c_ave)
+# draw closs
+x1 = np.linspace(0,300,300)
+loss1 = plt.plot(x1,c_ave, c='c', marker='o', ms= 2, lw=0.5, label='c_loss' )
+plt.ylim(0,6000)
+plt.xlabel('Epoch',fontsize = 12)
+plt.ylabel('c_loss', fontsize = 12)
+plt.grid(b = True, color = 'k', linestyle = '--', alpha = 0.2)
+plt.legend(fontsize = 10)
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+# ax.spines['bottom'].set_visible(False)
+ax.spines['right'].set_visible(False)
+# ax.spines['left'].set_visible(False)
+# plt.savefig('./loss_plot/tc100_td1_trainstep400/c_loss_ave.tif')
+plt.show()
 
 
-# 求gloss在每个epoch中的平均值
+# calculate the average gloss in every epoch
 g_ave = []
 g = np.load('./loss_plot/tc300_td1_trainstep1000/g_data.npy')
 for i in range(699):
@@ -41,13 +41,13 @@ for i in range(699):
     g_ave.append(ave)
 # print(g_ave)
 np.save('./loss_plot/tc300_td1_trainstep1000/g_ave.npy', g_ave)
-# 画gloss的图
+# draw gloss
 x2 = np.linspace(300,999,699)
 loss1 = plt.plot(x2,g_ave, c='orange', marker='o', ms= 2, lw=0.5, label='g_loss' )
 # plt.ylim(0,6000)
 plt.xlabel('Epoch',fontsize = 12)
 plt.ylabel('g_loss', fontsize = 12)
-plt.grid(b = True, color = 'w', linestyle = '--', alpha = 0.2)#alpha 为透明度
+plt.grid(b = True, color = 'w', linestyle = '--', alpha = 0.2)
 plt.legend(fontsize = 10)
 ax = plt.gca()
 ax.spines['top'].set_visible(False)
